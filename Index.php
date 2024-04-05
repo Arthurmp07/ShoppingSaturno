5d3f4dd17083964351bb0425e91aeb276af40f00
Pedro Santana <pedrohoxsantana@gmail.com>
2024-04-05 08:47:27 -0300

    Merge

--- Index.php
+++ Index.php
@@ -1,62 +1,25 @@
-<?php
-include_once 'header.php';
-?>
-<body>
-<!-- headerNav -->
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="headerNav/headernavstylesscripts/headerNav.css">
-<link rel="stylesheet" href="carrouseel/carrouselStylesScripts/carrousel.css">
-<link rel="stylesheet" href="main/main.css">
-    
-<!-- headerNav - End -->
-<!-- main -->
-    <main>
-        <article>
-            <div class="homeMainArticle">
-                <div class="homeMainArticleCarrossel">
-                    <div class="carrosel">
-                        <div class="carroselImgs">
-                          <div class="carroselImg" id="carroselImg1"> 
-                            <div class="carroselBtns">
-                              <a href="#carroselImg5" class="carroselBack"></a>
-                              <a href="#carroselImg2" class="carroselNext"></a>
-                            </div>
-                          </div>
-                          <div class="carroselImg" id="carroselImg2"> 
-                            <div class="carroselBtns">
-                              <a href="#carroselImg1" class="carroselBack"></a>
-                              <a href="#carroselImg3" class="carroselNext"></a>
-                            </div>
-                          </div>
-                          <div class="carroselImg" id="carroselImg3">
-                            <div class="carroselBtns">
-                              <a href="#carroselImg2" class="carroselBack"></a>
-                              <a href="#carroselImg4" class="carroselNext"></a>
-                            </div>
-                          </div>
-                          <div class="carroselImg" id="carroselImg4">
-                            <div class="carroselBtns">
-                              <a href="#carroselImg3" class="carroselBack"></a>
-                              <a href="#carroselImg5" class="carroselNext"></a>
-                            </div>
-                          </div>
-                          <div class="carroselImg" id="carroselImg5">
-                            <div class="carroselBtns">
-                              <a href="#carroselImg4" class="carroselBack"></a>
-                              <a href="#carroselImg1" class="carroselNext"></a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                </div>
-                <span class="homeMainArticleText">
-                    <h1>Objetivo do Projeto</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus consequuntur commodi! Odit mollitia vero saepe. Cumque sint temporibus nihil asperiores soluta nostrum, exercitationem eligendi pariatur cupiditate numquam fugiat molestias. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et nihil autem debitis unde perferendis, similique iste omnis. Minus optio illo magnam fugit mollitia! Soluta, itaque ullam. Esse earum iste at!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus consequuntur commodi! Odit mollitia vero saepe. Cumque sint temporibus nihil asperiores soluta nostrum, exercitationem eligendi pariatur cupiditate numquam fugiat molestias. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et nihil autem debitis unde perferendis, similique iste omnis. Minus optio illo magnam fugit mollitia! Soluta, itaque ullam. Esse earum iste at!</p>
-                </span>
-            </div>
-        </article>
-    </main>
-<!-- main - End -->
-</body>
-</html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./imgs/Logo - Saturno.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <!-- Links & Scripts / headerNav -->
+    <link rel="stylesheet" href="headerNav/headerNavStylesScripts/headerNav.css">
+    <script defer src="headerNav/headerNavStylesScripts/headerNav.js"></script>
+    <!-- Links & Scripts / headerNav - End -->
+    <!-- main -->
+    <link rel="stylesheet" href="./main/main.css">
+    <link rel="stylesheet" href="./carrouseel/carrouselStylesScripts/carrousel.css">
+    <!-- main - End -->
+        <!-- main -->
+        <link rel="stylesheet" href="./Footer/footer.css">
+        <!-- main - End -->
+    <title>Saturno</title>
+    </head>
+    <body>    
+    <?php
+        include_once 'headerNav.php';
+        include_once 'main.php';
+        include_once 'footer.php'
+    ?>
+    </body>
