5e8f5bc7cba0e6197cc18edddcb2f855440e3cca
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2024-06-21 20:22:43 -0300

    atualizacao

--- bDJson/participe.js
+++
@@ -1,8 +0,0 @@
-var participeSubmit = document.querySelector("#participeSubmit")
-var dataJson = JSON.parse(partipe);
-
-function submitJson(){
-    console.log(dataJson[0])
-}
-
-participeSubmit.addEventListener("click", submitJson)
